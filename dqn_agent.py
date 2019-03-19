a9503956f792b6460803255cd488a059895f3978
veldanie <davelasquez@gmail.com>
2019-03-18 21:56:04 -0500

    update agent

--- dqn_agent.py
+++ dqn_agent.py
@@ -14,7 +14,7 @@ class Agent():
     """Interacts with and learns from the environment."""
 
     def __init__(self, state_size, action_size, seed, fc1_units, fc2_units,
-    buffer_size=int(1e4), batch_size=64, gamma=0.99, tau=1e-2, lr=0.01, update_every=4):
+    buffer_size=int(1e5), batch_size=64, gamma=0.99, tau=1e-3, lr=5.e-4, update_every=4):
         """Initialize an Agent object.
 
         Params
@@ -94,11 +94,11 @@ class Agent():
         states, actions, rewards, next_states, dones = experiences
 
         # Get max predicted Q values (for next states) from target model
-        # Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
+        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
 
         #Double DQL
-        best_action = self.qnetwork_local(next_states).detach().argmax(1).unsqueeze(1)
-        Q_targets_next = torch.gather(self.qnetwork_target(next_states).detach(),1, best_action)
+        #best_action = self.qnetwork_local(next_states).detach().argmax(1).unsqueeze(1)
+        #Q_targets_next = torch.gather(self.qnetwork_target(next_states).detach(),1, best_action)
         # Compute Q targets for current states
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
 
