5251a79c824d56b308200c1fac12cba6cc2fb2b4
veldanie <davelasquez@gmail.com>
2019-03-25 18:44:36 -0500

    solved project

--- dqn_agent.py
+++ dqn_agent.py
@@ -94,11 +94,11 @@ class Agent():
         states, actions, rewards, next_states, dones = experiences
 
         # Get max predicted Q values (for next states) from target model
-        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
+        #Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
 
         #Double DQL
-        #best_action = self.qnetwork_local(next_states).detach().argmax(1).unsqueeze(1)
-        #Q_targets_next = torch.gather(self.qnetwork_target(next_states).detach(),1, best_action)
+        best_action = self.qnetwork_local(next_states).detach().argmax(1).unsqueeze(1)
+        Q_targets_next = torch.gather(self.qnetwork_target(next_states).detach(),1, best_action)
         # Compute Q targets for current states
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
 
